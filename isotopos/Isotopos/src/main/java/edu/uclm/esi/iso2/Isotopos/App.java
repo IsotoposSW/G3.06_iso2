59cf1f5478794bbfc6601e38d94cc8fb9f18d405
Antonio_GBA <Antonio.GarciaLabarrera@alu.uclm.es>
2016-10-19 12:57:58 +0200

    Segundo commit

---
+++ isotopos/Isotopos/src/main/java/edu/uclm/esi/iso2/Isotopos/App.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.iso2.Isotopos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
